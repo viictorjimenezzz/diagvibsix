644395af7334f55deb055f2fb5a61d3188141081
Victor Jimenez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2023-10-27 11:56:57 +0200

    Include explanations and allow for cache

--- diagvibsix/data/posterior_agreement.py
+++ diagvibsix/data/posterior_agreement.py
@@ -1,6 +1,10 @@
 import torch
 import csv
+import os
+import pickle
 from typing import Optional
+
+from numpy.random import seed as set_seed
 
 from .dataset.dataset import Dataset
 from .dataset.paint_images import Painter
@@ -8,13 +12,48 @@ from .dataset.config import OBJECT_ATTRIBUTES
 from .dataset.dataset_utils import get_mt_labels
 from .wrappers import TorchDatasetWrapper, get_per_ch_mean_std
 
+__all__ = ['EnvCSV', 'TorchDatasetCSV']
+
+def check_paths(csv_path, cache_path):
+    """Check if the provided paths for the cache and CSV files are valid."""
+
+    if (csv_path is None) and (cache_path is None): # no path is provided
+        raise ValueError('Either csv_path or cache_path must be specified.')
+        
+    elif (csv_path is not None) and (cache_path is not None): # both paths are provieded
+        if not os.path.exists(cache_path): # cache file not found
+            if not os.path.exists(csv_path): # csv file not found
+                raise ValueError('Cache file not found and no csv file was found.')
+            else:
+                print("Cache file not found. Generating images from the provided CSV.")
+        
+    elif cache_path is not None:  # only cache path is provided
+        if not os.path.exists(cache_path):
+            raise ValueError('Cache file not found.')
+            
+    elif csv_path is not None:  # only csv path is provided
+        if not os.path.exists(csv_path):
+            raise ValueError('CSV file not found.')
+
+
 class EnvCSV(Dataset):
-    """Subclass of DiagVib dataset to generate images from customized CSV specifications."""
+    """Subclass of DiagVib dataset to generate images from customized CSV specifications.
+    
+    Args:
+        mnist_preprocessed_path (str): Path to the processed MNIST dataset. If there is no such dataset, you can generate it by calling process_mnist.get_processed_mnist(mnist_dir).
+        csv_path (str): Path to the CSV file containing the dataset specifications.
+        t (Optional[str]): Type of dataset to be generated, corresponding to the key 'category' (e.g. 'train').
+        seed (Optional[int]): Random seed for the dataset generation.
+    
+    """
 
     def __init__(self,
                 mnist_preprocessed_path: str,
                 csv_path: str,
-                t: str = 'train'):
+                t: str = 'train',
+                seed: Optional[int] = 123):
+        
+        set_seed(seed) # numpy bc thats how files are generated
         
         self.painter = Painter(mnist_preprocessed_path)
 
@@ -57,16 +96,42 @@ class EnvCSV(Dataset):
     
 
 class TorchDatasetCSV(TorchDatasetWrapper):
+    """Wrapper class for the DiagVib dataset to generate images from customized CSV specifications.
+    
+    Args:
+        mnist_preprocessed_path (str): Path to the processed MNIST dataset. If there is no such dataset, you can generate it by calling process_mnist.get_processed_mnist(mnist_dir).
+        csv_path (Optional[str]): Path to the CSV file containing the dataset specifications, if no cache file is specified or found.
+        cache_path (Optional[str]): Path to the cache file containing the dataset. If the cache file does not exist, the dataset will be generated from the CSV file and then stored in the specified cache path.
+        t (Optional[str]): Type of dataset to be generated, corresponding to the key 'category' (e.g. 'train').
+        seed (Optional[int]): Random seed for the dataset generation.
+        normalization (Optional[str]): Normalization type. Defaults to 'z-score'.
+        mean (Optional[float]): Mean value for the normalization.
+        std (Optional[float]): Standard deviation value for the normalization.
+    
+    """
     def __init__(self,
                  mnist_preprocessed_path: str,
-                 csv_path: str,
+                 csv_path: Optional[str] = None,
+                 cache_path: Optional[str] = None,
                  t: str = 'train',
                  seed: Optional[int] = 123,
                  normalization: Optional[str] = 'z-score', 
                  mean: Optional[float] = None, 
                  std: Optional[float] = None):
         
-        self.dataset = EnvCSV(mnist_preprocessed_path, csv_path, t)
+        check_paths(csv_path, cache_path) # check if the provided paths are valid
+        
+        # Cache path is prioritized over csv_path.
+        # Load dataset object (uint8 images) from cache if available
+        if (cache_path is not None) and (os.path.exists(cache_path)):
+            with open(cache_path, 'rb') as f:
+                self.dataset = pickle.load(f)
+        else:
+            self.dataset = EnvCSV(mnist_preprocessed_path, csv_path, t, seed)
+            if cache_path is not None: # we want to store it as cache
+                with open(cache_path, 'wb') as f:
+                    pickle.dump(self.dataset, f, pickle.HIGHEST_PROTOCOL)
+        
         self.normalization = normalization
 
         self.mean, self.std = mean, std
